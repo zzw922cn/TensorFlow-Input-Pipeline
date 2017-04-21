339b712dde98985e959f4b4b1f53f5f160df76cf
zzw922cn <1422459603@qq.com>
2017-04-21 09:25:11 +0800

    udpate

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # TensorFlow-Input-Pipeline
-Input Pipeline Examples in TensorFlow
+Input Pipeline Examples based on multi-threads and FIFOQueue in TensorFlow, including mini-batching training.
+
+## Result
+
+[-] Run `python big_input.py`, you can get:
+
+```
+[array([ 6, 10], dtype=int32), array([[[ 1. ,  2.1,  3.5],
+        [ 4. ,  5. ,  6. ]],
+
+       [[ 1. ,  2. ,  3.5],
+        [ 4. ,  5. ,  6. ]]])]
+[array([8], dtype=int32), array([[[ 1. ,  2. ,  3.5],
+        [ 4. ,  5.5,  6. ]]])]
+```
+
+[-] Run `python small_input.py`, you can get:
+
+```
+[array([[0, 1],
+       [6, 7]], dtype=int32), 16]
+==============================
+[array([[2, 3],
+       [4, 5]], dtype=int32), 16]
+==============================
+[array([[10, 11],
+       [12, 13]], dtype=int32), 144]
+==============================
+[array([[8, 9]], dtype=int32), 81]
+==============================
+pass
+```
--- big_input.py
+++ big_input.py
@@ -4,12 +4,15 @@ import os
 import numpy as np
 ''' This is for big dataset, so we can store data into tfrecords '''
 
+# for any data type except int
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
+# for int data type
 def _int64_feature(value):
   return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
+## write tfrecord file
 class RecordWriter(object):
   def __init__(self, path):
     self.path = path
@@ -22,17 +25,18 @@ class RecordWriter(object):
       feature_dict = {}
       for i in range(feature_num):
         feature = content[i]
-        if not isinstance(feature, int):
+        if isinstance(feature, int):
+          feature_dict['feature'+str(i+1)]=_int64_feature(feature)
+        else:
           feature_raw = np.array(feature).tostring()
           feature_dict['feature'+str(i+1)]=_bytes_feature(feature_raw)
-        else:
-          feature_dict['feature'+str(i+1)]=_int64_feature(feature)
       features_to_write = tf.train.Example(features=tf.train.Features(feature=feature_dict))
       writer.write(features_to_write.SerializeToString())
       writer.close()
       print('Record has been writen:'+tfrecords_filename)
 
 
+## read tfrecord file
 def read(filename_queue, feature_num=2, dtypes=[list, int]):
   reader = tf.TFRecordReader()
   _, serialized_example = reader.read(filename_queue)
@@ -48,8 +52,10 @@ def read(filename_queue, feature_num=2, dtypes=[list, int]):
       features=feature_dict)
   return features
 
+#======================================================================================
 ## test code
 rw = RecordWriter('/home/pony/github/seq2seq_tf/seq2seq_tf/pipeline/')
+# you can take a1, a2, a3 as training samples, and take b1, b2, b3 as labels
 a1 = np.array([[1,2,3.5],[4,5,6]])
 a2 = np.array([[1,2,3.5],[4,5.5,6]])
 a3 = np.array([[1,2.1,3.5],[4,5,6]])
@@ -61,8 +67,12 @@ rw.write([b2,a2], 'test2.tfrecords')
 rw.write([b3,a3], 'test3.tfrecords')
 
 fq = tf.train.string_input_producer(['test1.tfrecords', 'test2.tfrecords', 'test3.tfrecords'], 1, shuffle=False)
+
+# read tfrecords, you should specify the data type of each feature
+# egs, int, float, list, string
 features = read(fq, dtypes=[int, list])
 aa = tf.cast(features['feature1'], tf.int32)
+
 bb = tf.decode_raw(features['feature2'], tf.float64)
 bb = tf.reshape(bb, [2,3])
 
@@ -79,4 +89,3 @@ for i in range(num_batchs):
 
 coord.request_stop()
 coord.join(threads)
-
